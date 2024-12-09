4ff47bbc37180c0f2cf16cb8776cb6a250036c21
Vinicius Pinho <vinicius.pinho@zoppy.com.br>
2024-12-09 01:38:11 -0300

    refactor infrastructure

--- src/dominio/domain.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from "@nestjs/common";
-import { Cliente } from "./entidades/cliente.entidade";
-
-@Module({
-  imports: [],
-  providers: [Cliente],
-  exports: [Cliente],
-})
-export class DomainModule {}
